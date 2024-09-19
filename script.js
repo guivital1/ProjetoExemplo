365830f8553b47b34c30836dedf4ed4112aa3640
Guilherme Vital <guivitalrodrigues@gmail.com>
2024-09-19 11:34:04 -0300

    Verificando o tipo da variavel

--- script.js
+++ script.js
@@ -4,45 +4,67 @@
 
 // declaração definida ou com valores
 var nome = "fiap";
-console.log(nome)
+console.log(nome);
 
 let nome1 = "Gui";
 console.log(nome1)
 
 const nome2 = "javaScript";
-console.log(nome2)
+console.log(nome2);
 
 
 //declaraçao indefinida
 let nome3;
-console.log(nome3)
+console.log(nome3);
 
 //declaração vazia
 let disciplina = null
-console.log(disciplina)
+console.log(disciplina);
 
 // tipos de variaveis 
 
 // inteiros 
 let idade = 18;
-console.log(idade)
+console.log(idade);
 
 // flutuantes ou com casas decimais
 let valor = 6.40;
-console.log(valor)
+console.log(valor);
 
 //string(textos) con aspas duplas, gira aspas 
 let texto ="Fiap";
-console.log(texto)
+console.log(texto);
 
 // booleanas - true or false
 
 let aluno = true;
-console.log(aluno)
+console.log(aluno);
 
 // array - conjunto de informações
 
 let tecnologias = ["Java", "C#", "HTML", "css"]
-console.log(tecnologias)
+console.log(tecnologias);
+
+// conversões
+// conversa de float para inteiro
+
+let numeroFloat = 123.456;
+console.log(parseInt(numeroFloat));
+
+//converta de string para float
+
+let numeroString = "654.321"
+console.log(parseFloat(numeroString))
+
+// converta de numero inteiro para string
+
+let numeroInteiro = 789
+console.log(numeroInteiro.toString())
+
+// ver o tipo da variavel
+
+let verTipo = typeof true
+console.log(verTipo)
+
 
 
