46b9733c91fe5c5ef47e5ac4b459a3a983f49b95
Guilherme Vital <guivitalrodrigues@gmail.com>
2024-09-19 11:07:51 -0300

    Declarando os tipos de dados var, let, const

---
+++ script.js
@@ -0,0 +1,12 @@
+// alert("Olá Mundo - externo")
+
+// declarando as variaveis
+
+var nome = "fiap";
+console.log(nome)
+
+let nome1 = "Gui";
+console.log(nome1)
+
+const nome2 = "javaScript";
+console.log(nome2)
