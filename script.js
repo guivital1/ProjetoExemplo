c686e2fd7a7959bb57eddef917ef3735ee1b0879
Guilherme Vital <guivitalrodrigues@gmail.com>
2024-09-19 11:23:09 -0300

    Criando tipos de variaveis

--- script.js
+++ script.js
@@ -21,3 +21,28 @@ console.log(nome3)
 let disciplina = null
 console.log(disciplina)
 
+// tipos de variaveis 
+
+// inteiros 
+let idade = 18;
+console.log(idade)
+
+// flutuantes ou com casas decimais
+let valor = 6.40;
+console.log(valor)
+
+//string(textos) con aspas duplas, gira aspas 
+let texto ="Fiap";
+console.log(texto)
+
+// booleanas - true or false
+
+let aluno = true;
+console.log(aluno)
+
+// array - conjunto de informações
+
+let tecnologias = ["Java", "C#", "HTML", "css"]
+console.log(tecnologias)
+
+
